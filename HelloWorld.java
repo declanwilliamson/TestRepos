5c8353f2457f5967f09c473db2396d4f836dbf05
declanwilliamson <declanwilliamson@hotmail.com>
2018-11-13 15:19:57 +0000

    new line

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 		// Prints "Hello, World" to the terminal window.
 		System.out.println("Hello World");
 		System.out.println("Well hello to anyone really");
+		System.out.println("Adding another stream");
 	}
 }
